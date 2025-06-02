9a7c3182ce8708cc4cc988cd63ff26cd162e6cf1
Jineesh <jineeshjohn@gmail.com>
2025-06-02 02:21:26 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+ import './App.scss';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default App;
