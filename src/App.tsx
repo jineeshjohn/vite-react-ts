af411f172cf74ab22406c3af3a0afad1d8132c8e
Jineesh <jineeshjohn@gmail.com>
2025-09-19 03:38:12 +0000

    add reach router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React, { useState } from 'react';
+import { Router, Link } from '@reach/router';
 import QuoteCard from './components/QuoteCard';
 
-export function App() {
+function Home() {
   return <QuoteCard />;
 }
 
-export default App;
+function About() {
+  return <h2>About Page</h2>;
+}
+
+function Dashboard() {
+  return <h2>Contact Page</h2>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
+      </nav>
+      <Router>
+        <Home path="/" />
+        <Dashboard path="dashboard" />
+      </Router>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div> testing </div>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
